6ee30946cb46db6758e79ebb9453aa0b2b9fdf74
RedPaladin7 <ishans7325@gmail.com>
2025-12-28 19:58:03 +0530

    28th Dec 1

---
+++ client/client_test.go
@@ -0,0 +1,45 @@
+package client
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"testing"
+	"time"
+)
+
+func TestNewClient1(t *testing.T) {
+	c, err := New("localhost:5001")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// time.Sleep(time.Second)
+	if err := c.Set(context.TODO(), "foo", "1"); err != nil {
+		log.Fatal(err)
+	}
+	val, err := c.Get(context.TODO(), "foo") 
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(val)
+
+}
+
+func TestNewClient(t *testing.T) {
+	c, err := New("localhost:5001")
+	if err != nil {
+		log.Fatal(err)
+	}
+	time.Sleep(time.Second)
+	for i := 0; i < 5; i++ {
+		fmt.Println("SET => ", fmt.Sprintf("bar_%d", i))
+		if err := c.Set(context.TODO(), fmt.Sprintf("foo_%d", i), fmt.Sprintf("bar_%d", i)); err != nil {
+			log.Fatal(err)
+		}
+		val, err := c.Get(context.TODO(), fmt.Sprintf("foo_%d", i)) 
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("GET => ", val)
+	}
+}
